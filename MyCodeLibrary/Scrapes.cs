395a67df908140ed3d234bb578418ebb0a6cba15
theolamide <ajadidan@gmail.com>
2020-10-14 23:21:58 -0400

    Add project files.

---
+++ MyCodeLibrary/Scrapes.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace MyCodeLibrary
+{
+    public class Scrape
+    {
+        public string ScrapeWebpage(string url)
+        {
+            return GetWebPage(url);
+        }
+
+        public string ScrapeWebpage(string url, string filepath)
+        {
+            string reply = GetWebPage(url);
+
+            File.WriteAllText(filepath, reply);
+            return reply;
+
+        }
+
+        private string GetWebPage(string url)
+        {
+            WebClient client = new WebClient();
+            return client.DownloadString(url);
+        }
+    }
+
+}
